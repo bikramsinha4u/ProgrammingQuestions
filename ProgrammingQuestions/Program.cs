271eeb538f9a7da6058c9ca9a801a52bd9490d2b
Bikram Sinha <bikram.sinha@siemens-healthineers.com>
2021-05-19 21:54:10 +0530

    Add project files.

---
+++ ProgrammingQuestions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
