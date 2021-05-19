443fe70aa7d764436487d9ef8e9cb95785c41e66
Bikram Sinha <bikram.sinha@siemens-healthineers.com>
2021-05-19 22:17:03 +0530

    Linear Search

---
+++ Searching/LinearSearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Searching
+{
+    class LinearSearch
+    {
+        public int[] InputArray { get; set; } = new int[] { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
+        
+        public int Key { get; set; } = 110;
+
+        public int Search(int[] arr, int key)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Searching/StartupProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Searching
+{
+    class StartupProgram
+    {
+        static void Main(string[] args)
+        {
+            LinearSearch linearSearch = new LinearSearch();
+            int output = linearSearch.Search(linearSearch.InputArray, linearSearch.Key);
+            Console.WriteLine($"Liner Search Output: {output}");
+
+
+
+            Console.ReadKey(); // To keep the output console persist.
+        }
+    }
+}
