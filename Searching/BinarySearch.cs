d1ff398a7aeedc59dda7dd01f9df96d0dfad44d2
Bikram Sinha <bikram.sinha@siemens-healthineers.com>
2021-05-22 23:12:29 +0530

    Binary Search

---
+++ Searching/BinarySearch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Searching
+{
+    class BinarySearch
+    {
+        public int[] InputArray { get; set; } = new int[] { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
+
+        public int Key { get; set; } = 60;
+
+        public int Search(int[] arr, int key)
+        {
+            Array.Sort(arr); // { 10, 20, 30, 50, 60, 80, 100, 110, 130, 170 }
+
+            int lower = 0;
+            int upper = arr.Length - 1;
+            
+            while(upper >= lower)
+            {
+                int mid = (lower + upper) / 2;
+                
+                if (arr[mid] == key)
+                {
+                    return mid;
+                }
+                
+                if (key > arr[mid])
+                {
+                    lower = mid + 1;
+                }
+                else // if (key < mid)
+                {
+                    upper = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public int SearchRecursive(int[] arr, int key, int lower, int upper)
+        {
+            if (upper >= lower)
+            {
+                int mid = (lower + upper) / 2;
+                if (key == arr[mid])
+                {
+                    return mid;
+                }
+
+                if (key < arr[mid])
+                {
+                    return SearchRecursive(arr, key, lower, mid - 1);
+                }
+                else
+                {
+                    return SearchRecursive(arr, key, mid + 1, upper);
+                }
+            }
+            return -1;
+        }
+    }
+}
+/*
+ * Auxiliary Space: O(1) in case of iterative implementation.
+ * In the case of recursive implementation, O(Logn) recursion call stack space.
+ */
--- Searching/StartupProgram.cs
+++ Searching/StartupProgram.cs
@@ -6,11 +6,18 @@ namespace Searching
     {
         static void Main(string[] args)
         {
-            LinearSearch linearSearch = new LinearSearch();
-            int output = linearSearch.Search(linearSearch.InputArray, linearSearch.Key);
-            Console.WriteLine($"Liner Search Output: {output}");
+            //LinearSearch linearSearch = new LinearSearch();
+            //int linearSearchOutput = linearSearch.Search(linearSearch.InputArray, linearSearch.Key);
+            //Console.WriteLine($"Liner Search Output: {linearSearchOutput}");
 
+            //BinarySearch binarySearch = new BinarySearch();
+            //int binarySearchOutput = binarySearch.Search(binarySearch.InputArray, binarySearch.Key);
+            //Console.WriteLine($"Binary Search Output: {binarySearchOutput}");
 
+            BinarySearch binarySearch = new BinarySearch();
+            Array.Sort(binarySearch.InputArray);
+            int binarySearchOutput = binarySearch.SearchRecursive(binarySearch.InputArray, binarySearch.Key, 0, binarySearch.InputArray.Length - 1);
+            Console.WriteLine($"Binary Search recursive Output: {binarySearchOutput}");
 
             Console.ReadKey(); // To keep the output console persist.
         }
